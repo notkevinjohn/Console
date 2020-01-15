d0a101fd8efe0720959f59798690013a78af83fc
Kevin John <notkevinjohn@gmail.com>
2020-01-15 12:25:46 -0800

    updated for python3

--- Console.py
+++ Console.py
@@ -10,89 +10,89 @@ import sys
 
 class Console(ScrollView):
 
-	Builder.load_file("/home/sensys/work/sensys/tests/Console/Console.kv")
-	rowHeight = 22
+    Builder.load_file("/home/sensys/work/sensys/tests/Console/Console.kv")
+    rowHeight = 22
 
-	pallet = {
-		'yellow':'FFFF00',
-		'white':'FFFFFF',
-		'blue':'0099FF',
-		'green':'00FF00',
-		'red':'FF0000',
-		'purple':'FF00FF',
-		'orange':'FFBB00',
-		'gray':'999999'
-	}
+    pallet = {
+            'yellow':'FFFF00',
+            'white':'FFFFFF',
+            'blue':'0099FF',
+            'green':'00FF00',
+            'red':'FF0000',
+            'purple':'FF00FF',
+            'orange':'FFBB00',
+            'gray':'999999'
+    }
 
-	def __init__(self, logDir, logFile, **kwargs):
-		super(Console, self).__init__(**kwargs)
-		self.consoleGrid = ConsoleGrid()
-		self.consoleGrid.height = 0
-		self.add_widget(self.consoleGrid)
-		self.file = logFile
-		self.dir = logDir
+    def __init__(self, logDir, logFile, **kwargs):
+        super(Console, self).__init__(**kwargs)
+        self.consoleGrid = ConsoleGrid()
+        self.consoleGrid.height = 0
+        self.add_widget(self.consoleGrid)
+        self.file = logFile
+        self.dir = logDir
 
-	def clear(self):
-		self.consoleGrid.clear_widgets();
-		self.consoleGrid.height = 0
+    def clear(self):
+        self.consoleGrid.clear_widgets();
+        self.consoleGrid.height = 0
 
-	def message(self, text, color='default'):
-		self.addText(text, color, True, True, False)
+    def message(self, text, color='default'):
+        self.addText(text, color, True, True, False)
 
-	def message_temp(self, text, color='default'):
-		self.addText(text, color, True, True, True)
+    def message_temp(self, text, color='default'):
+        self.addText(text, color, True, True, True)
 
-	def note(self, text, color='default'):
-		self.addText(text, color, True, False, True)
+    def note(self, text, color='default'):
+        self.addText(text, color, True, False, False)
 
-	def note_temp(self, text, color):
-		self.addText(text, color, True, False, True)
+    def note_temp(self, text, color):
+        self.addText(text, color, True, False, True)
 
-	def log(self, text):
-		self.addText(text, 'default', False, True, False)	
-	
+    def log(self, text):
+        self.addText(text, 'default', False, True, False)
 
-	def addText(self, text, color, Console, Log, Temp):
-		if Log:
-			now = datetime.datetime.now()
-			if not os.path.isdir(self.dir):
-				print "Error: log directory "+self.dir+" does not exist!"
-				return
 
-			path = self.dir+"/"+self.file
-			f = open(path, "a")
-			f.write(now.strftime("%Y-%m-%d %H:%M")+": "+str(text)+"\n")
-			f.close()
+    def addText(self, text, color, Console, Log, Temp):
+        if Log:
+            now = datetime.datetime.now()
+            if not os.path.isdir(self.dir):
+                print ("Error: log directory "+self.dir+" does not exist!")
+                return
 
-		if Console:
-			for label in self.consoleGrid.children:
-				if label.Temp == True:
-					self.removeLabel(label)
-	
-			label = ConsoleLabel(Temp, font_size=self.rowHeight - 4)
-			label.height = self.rowHeight
+            path = self.dir+"/"+self.file
+            f = open(path, "a")
+            f.write(now.strftime("%Y-%m-%d %H:%M")+": "+str(text)+"\n")
+            f.close()
 
-			color = color.lower().strip()
-			if color in self.pallet:
-				color = self.pallet[color]
-			else:
-				color = self.pallet['white']
-			label.text = '[color='+color+']'+str(text)+'[/color]'		
-			self.addLabel(label)		
-	
-	def addLabel(self, label):
-		self.consoleGrid.height += self.rowHeight
-		self.consoleGrid.add_widget(label)
-		self.scroll_to(label)
+        if Console:
+            for label in self.consoleGrid.children:
+                if label.Temp == True:
+                    self.removeLabel(label)
 
-	def removeLabel(self, label):
-		self.consoleGrid.height -= self.rowHeight
-		self.consoleGrid.remove_widget(label)
+            label = ConsoleLabel(Temp, font_size=self.rowHeight - 4)
+            label.height = self.rowHeight
+
+            color = color.lower().strip()
+            if color in self.pallet:
+                color = self.pallet[color]
+            else:
+                color = self.pallet['white']
+            label.text = '[color='+color+']'+str(text)+'[/color]'
+            self.addLabel(label)
+
+    def addLabel(self, label):
+        self.consoleGrid.height += self.rowHeight
+        self.consoleGrid.add_widget(label)
+        self.scroll_to(label)
+
+    def removeLabel(self, label):
+        self.consoleGrid.height -= self.rowHeight
+        self.consoleGrid.remove_widget(label)
 
 class ConsoleLabel(Label):
-	def __init__(self, Temp, *args, **kwargs):
-		super(ConsoleLabel, self).__init__(*args, **kwargs)
-		self.Temp = Temp
+    def __init__(self, Temp, *args, **kwargs):
+        super(ConsoleLabel, self).__init__(*args, **kwargs)
+        self.Temp = Temp
 
 class ConsoleGrid(GridLayout):
-	pass
+    pass
--- Demo.py
+++ Demo.py
@@ -7,54 +7,90 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from Console import Console
 import threading
+from random import randint
 
 
 class Application(App):
 		def build(self):
-			boxlayout = BoxLayout(orientation='vertical')			
+			boxlayout = BoxLayout(orientation='vertical')
 			logDir = "/home/sensys/work/sensys/tests/Console"
 			logFile = "test.log"
 			self.console = Console(logDir,logFile)
-			boxlayout.add_widget(self.console)			
+			boxlayout.add_widget(self.console)
 			return boxlayout
 
-def write():
-	for i in range(0,1):
+def demo():
+	time.sleep(0.5)
+	application.console.message("Here is a message in white")
+	time.sleep(0.5)
+	application.console.message("Here is a message in red","Red")
+	time.sleep(0.5)
+	application.console.message("Here is a message in yellow","Yellow")
+	time.sleep(0.5)
+	application.console.message("Here is a message in green","green")
+	time.sleep(0.5)
+	application.console.message("Here is a message in blue","blue")
+	time.sleep(0.5)
+	application.console.message("Here is a message in purple","purple")
+	time.sleep(0.5)
+	application.console.message("Here is a message in orange","orange")
+	time.sleep(1)
+	application.console.message_temp("Here is a temp message 1/3","gray")
+	time.sleep(1)
+	application.console.message_temp("Here is a temp message 2/3","gray")
+	time.sleep(1)
+	application.console.message_temp("Here is a temp message 3/3","gray")
+	time.sleep(1)
+	application.console.note("This will not show in the log")
+	time.sleep(1)
+	application.console.note_temp("This will not show in the log, and it won't last here", "red")
+	time.sleep(1)
+	application.console.log("This will only show in the log")
+	time.sleep(1)
+	application.console.message("Complete","green")
+	time.sleep(5)
+	application.console.clear()
+
+def example():
+	time.sleep(0.5)
+	application.console.message("Starting","green")
+	application.console.note("Initializing Application...","blue")
+	time.sleep(1)
+	application.console.note("Application Initialized","blue")
+	time.sleep(1)
+	application.console.note("Loading Data...","blue")
+	for i in range(0,21):
+		application.console.note_temp(str(i*5)+"% loaded","yellow")
+		time.sleep(0.25)
+	time.sleep(0.5)
+	application.console.note("Data Loaded","blue")
+	application.console.note("Connecting To Device", "blue")
+	time.sleep(1)
+	application.console.message("Failed To Connect to Device, Retrying...", "red")
+	time.sleep(1)
+	application.console.message("Connected to Device", "green")
+	application.console.note("Downloading Data...", "blue")
+	for i in range(0,5):
+		data = []
+		for i in range(0,20):
+			data.append(str(randint(0,256)))
+		application.console.message("["+",".join(data)+"]", "grey")
 		time.sleep(0.5)
-		application.console.message("Here is a message in white")	
-		time.sleep(0.5)
-		application.console.message("Here is a message in red","Red")	
-		time.sleep(0.5)
-		application.console.message("Here is a message in yellow","Yellow")	
-		time.sleep(0.5)
-		application.console.message("Here is a message in green","green")	
-		time.sleep(0.5)
-		application.console.message("Here is a message in blue","blue")
-		time.sleep(0.5)
-		application.console.message("Here is a message in purple","purple")
-		time.sleep(0.5)
-		application.console.message("Here is a message in orange","orange")
-		time.sleep(1)
-		application.console.message_temp("Here is a temp message 1/3","gray")
-		time.sleep(1)
-		application.console.message_temp("Here is a temp message 2/3","gray")
-		time.sleep(1)
-		application.console.message_temp("Here is a temp message 3/3","gray")
-		time.sleep(1)
-		application.console.note("This will not show in the log")
-		time.sleep(1)
-		application.console.note_temp("This will not show in the log, and it won't last here", "red")
-		time.sleep(1)
-		application.console.log("This will only show in the log")
-		time.sleep(1)
-		application.console.message("Complete","green")	
-		time.sleep(5)
-		application.console.clear()	
-	
+	application.console.note("Data Downloaded", "blue")
+	application.console.note("Proccessing Data...", "blue")
+	for i in range(0,21):
+                application.console.note_temp(str(i*5)+"% Processed","yellow")
+                time.sleep(0.25)
+        time.sleep(0.5)
+	application.console.message("Final Result: 42","purple")
+	time.sleep(0.5)
+	application.console.message("Complete","green")
+
+
 if __name__ == '__main__':
-        Window.size = (1280,800)
+        Window.size = (800,600)
         Window.fullscreen = False
         application = Application()
-        t1 = threading.Thread(target=write, args=()) 
+        t1 = threading.Thread(target=example, args=()) 
         t1.start()
         application.run()
